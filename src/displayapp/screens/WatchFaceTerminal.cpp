1d645998d24afe1e9916d4cc05d106c622ab90ef
lukecorp14 <44783970+lukecorp14@users.noreply.github.com>
2023-06-26 13:29:11 -0400

    Update WatchFaceTerminal.cpp

--- src/displayapp/screens/WatchFaceTerminal.cpp
+++ src/displayapp/screens/WatchFaceTerminal.cpp
@@ -138,9 +138,9 @@ void WatchFaceTerminal::Refresh() {
   heartbeatRunning = heartRateController.State() != Controllers::HeartRateController::States::Stopped;
   if (heartbeat.IsUpdated() || heartbeatRunning.IsUpdated()) {
     if (heartbeatRunning.Get()) {
-      lv_label_set_text_fmt(heartbeatValue, "[L_HR]#ee3311 %d bpm#", heartbeat.Get());
+      lv_label_set_text_fmt(heartbeatValue, "[TEST]#ee3311 %d bpm#", heartbeat.Get());
     } else {
-      lv_label_set_text_static(heartbeatValue, "[L_HR]#ee3311 ---#");
+      lv_label_set_text_static(heartbeatValue, "[TEST]#ee3311 ---#");
     }
   }
 
